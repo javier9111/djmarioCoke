13bbe62bcc2dbdb910eba7d976208c3035a8cb8c
Javrobles <ringo.larios@gmail.com>
2025-05-26 17:09:55 -0600

    Add Tailwind CSS and Flowbite integration with initial layout and components

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+export default {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#FFD700', // Dorado
+          background: '#000000', // Negro
+          text: '#FFFFFF', // Blanco
+        },
+      },
+    },
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+    plugins: [],
+  };
+  
