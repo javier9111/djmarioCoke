29d611df2908983a05722660014b51bed01514d9
Javrobles <ringo.larios@gmail.com>
2025-05-30 13:09:42 -0600

    adding config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: 'https://javier9111.github.io', // o tu dominio personalizado
   base: '/djmarioCoke/', // Si usas repositorio, no olvides la barra final
+  outDir: './dist',
   trailingSlash: 'ignore',
+  build: {
+    format: 'directory'
+  },
     vite: {
         plugins: [tailwindcss()],
       },
