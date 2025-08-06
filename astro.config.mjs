6462cfea74b28acba7e83585264265855ea5d18c
Coveran <ringo.larios@gmail.com>
2025-08-06 12:31:13 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://javier9111.github.io', // o tu dominio personalizado
-  base: '/djmarioCoke/', // Si usas repositorio, no olvides la barra final
+  base: '/djmarioCoke', // Si usas repositorio, no olvides la barra final
   outDir: './dist',
   trailingSlash: 'ignore',
   build: {
