f81dfa5342031d7252fab5d762e42eb02faf992f
Javrobles <ringo.larios@gmail.com>
2025-10-06 18:06:38 -0600

    adding new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   // site: 'https://javier9111.github.io', // o tu dominio personalizado
   // base: '/djmarioCoke', // Si usas repositorio, no olvides la barra final
   site: 'https://djmariococa.netlify.app', // opcional
-  base: '/', // ¡importante! sirve desde la raíz
+  base: '', // ¡importante! sirve desde la raíz
   outDir: './dist',
   trailingSlash: 'ignore',
   build: {
